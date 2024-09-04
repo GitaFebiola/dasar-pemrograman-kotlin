0ad037ae77459fae6f736afbeb6d85a8fc5c6990
GitaFebiola <gitaafebiolaa@gmail.com>
2024-09-04 13:22:34 +0700

    Add BlokdanKomentar.kt

---
+++ src/main/kotlin/BlokdanKomentar.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    println("Hello, Ini Program Pertama Saya di Kotlin!")
+//Contoh komentar satu baris
+    /*
+    Contoh multi baris
+     */
+    /**
+     * kotlin Doc --> baris ini untuk mendokumentasikan kode program
+     */
+}
