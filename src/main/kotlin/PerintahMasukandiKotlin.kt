fa273170738dfe39cf070d33ad0b6c2ada4f3fd1
GitaFebiola <gitaafebiolaa@gmail.com>
2024-09-06 07:28:40 +0700

    Add PerintahMasukandiKotlin.kt

---
+++ src/main/kotlin/PerintahMasukandiKotlin.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    println("Hi I'm Febiola, the virtual parroot, and i love to repeat!");
+    println("Type something in: ");
+    var input: String
+    input = readLine()!!
+    var output: String
+    output = input + ", " + input + "!"
+    println(output)
+}
